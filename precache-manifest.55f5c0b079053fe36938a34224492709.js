c880dbc72619c73708904ba5a878836b2f83e54f
Giovanni Luro <giovanni.luro@outlook.com>
2020-07-11 03:38:24 -0300

    Updates

---
+++ precache-manifest.55f5c0b079053fe36938a34224492709.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00471a0df3ec97181c123941378f3b43",
+    "url": "/card-memory-game/index.html"
+  },
+  {
+    "revision": "520873ce5942de374eac",
+    "url": "/card-memory-game/static/js/2.d2e142d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/card-memory-game/static/js/2.d2e142d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a96a0ac4389020200c65",
+    "url": "/card-memory-game/static/js/main.b8f52b32.chunk.js"
+  },
+  {
+    "revision": "1571f5c63d559927f576",
+    "url": "/card-memory-game/static/js/runtime-main.d53b6313.js"
+  }
+]);
